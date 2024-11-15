dc7f21478569a62dd21ee6f5e4fac4f86d6ea133
Muhammad Rehan <sheikhrehan877@gmail.com>
2024-11-15 23:03:16 +0500

    Create Way Too Long Words.py

---
+++ Way Too Long Words.py
@@ -0,0 +1,21 @@
+#Way Too Long Words
+#https://codeforces.com/problemset/problem/71/A
+
+x=int(input())
+for i in range (x):
+    y=str(input())
+    l=len(y)
+    a=y[0:1]
+    b=y[1:l-1]
+    c=y[l-1:l]
+    if l<=10:
+        print (y)
+    else:
+        count=0
+        for j in a:
+            first=j
+        for j in b:
+            count=count+1
+        for j in c:
+            last=j
+        print (f'{first}{count}{last}')
