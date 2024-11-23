ccf58308f630003935cb4cedd77fa912d05f2761
Muhammad Rehan <sheikhrehan877@gmail.com>
2024-11-23 23:33:59 +0500

    Create Boy or Girl.py

---
+++ Boy or Girl.py
@@ -0,0 +1,115 @@
+#Boy or Girl /800
+#https://codeforces.com/problemset/problem/236/A
+
+name=input()
+a=b=c=d=e=f=g=h=i=j=k=l=m=n=o=p=q=r=s=t=u=v=w=x=y=z=0
+for ch in (name):
+    if ch=='a':
+        a+=1
+    elif ch=='b':
+        b+=1
+    elif ch=='c':
+        c+=1
+    elif ch=='d':
+        d+=1
+    elif ch=='e':
+        e+=1
+    elif ch=='f':
+        f+=1
+    elif ch=='g':
+        g+=1
+    elif ch=='h':
+        h+=1
+    elif ch=='i':
+        i+=1
+    elif ch=='j':
+        j+=1
+    elif ch=='k':
+        k+=1
+    elif ch=='l':
+        l+=1
+    elif ch=='m':
+        m+=1
+    elif ch=='n':
+        n+=1
+    elif ch=='o':
+        o+=1
+    elif ch=='p':
+        p+=1
+    elif ch=='q':
+        q+=1
+    elif ch=='r':
+        r+=1
+    elif ch=='s':
+        s+=1
+    elif ch=='t':
+        t+=1
+    elif ch=='u':
+        u+=1
+    elif ch=='v':
+        v+=1
+    elif ch=='w':
+        w+=1
+    elif ch=='x':
+        x+=1
+    elif ch=='y':
+        y+=1
+    elif ch=='z':
+        z+=1
+count = 0
+if a!=0:
+    count+=1
+if b!=0:
+    count+=1
+if c!=0:
+    count+=1
+if d!=0:
+    count+=1
+if e!=0:
+    count+=1
+if f!=0:
+    count+=1
+if g!=0:
+    count+=1
+if h!=0:
+    count+=1
+if i!=0:
+    count+=1
+if j!=0:
+    count+=1
+if k!=0:
+    count+=1
+if l!=0:
+    count+=1
+if m!=0:
+    count+=1
+if n!=0:
+    count+=1
+if o!=0:
+    count+=1
+if p!=0:
+    count+=1
+if q!=0:
+    count+=1
+if r!=0:
+    count+=1
+if s!=0:
+    count+=1
+if t!=0:
+    count+=1
+if u!=0:
+    count+=1
+if v!=0:
+    count+=1
+if w!=0:
+    count+=1
+if x!=0:
+    count+=1
+if y!=0:
+    count+=1
+if z!=0:
+    count+=1
+if count%2==0:
+    print ('CHAT WITH HER!')
+else:
+    print ('IGNORE HIM!')
